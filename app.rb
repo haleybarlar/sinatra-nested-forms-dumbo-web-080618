c08a62a8f7f4951057c46be0904c2e5d001e62da
haleybarlar <hbarlar1@gmail.com>
2018-08-29 10:17:55 -0400

    Submit

--- app.rb
+++ app.rb
@@ -4,6 +4,22 @@ module FormsLab
   class App < Sinatra::Base
 
     # code other routes/actions here
+get '/' do
+  erb :root
+end
 
+get '/new' do
+  erb :"pirates/new"
+end
+
+post '/pirates' do
+  @pirate1 = Pirate.new(params[:pirate])
+  params[:pirate][:ships].map do |all|
+    Ship.new(all)
   end
+  @ship1 = Ship.all
+  erb :'pirates/show'
+end
+
+end
 end
